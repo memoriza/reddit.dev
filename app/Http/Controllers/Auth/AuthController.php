7e285b4fc8e4457519ed7eb9e331e0d0aa1b6917
Joshua Williams <joshuamw322@gmail.com>
2017-05-02 11:17:30 -0500

    added re-direct for logging out

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -31,6 +31,8 @@ class AuthController extends Controller
 
     protected $loginPath = '/login';
 
+    protected $redirectAfterLogout = '/posts';
+
 
     /**
      * Create a new authentication controller instance.
@@ -68,7 +70,7 @@ class AuthController extends Controller
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
-            'password' => bcrypt($data['password']),
+            'password' => ($data['password']),
         ]);
     }
 }
